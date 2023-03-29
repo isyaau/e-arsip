a45e0734a58e616d0b85036da57fab5995d25871
Isyaau <isak.akhsanil@gmail.com>
2023-03-29 13:23:31 +0700

    edit title login

--- app/Views/login.php
+++ app/Views/login.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Rental Mobil</title>
+    <title>E-Arsip</title>
 
     <!-- Custom fonts for this template-->
     <link href="<?= base_url(); ?>/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
